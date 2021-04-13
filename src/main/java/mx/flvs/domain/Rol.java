8fc5507363cf9ebd7025a68023fec3df13a5b351
Lazcano Valdez César Ricardo <la384541@uaeh.edu.mx>
2021-04-13 16:54:16 -0500

    Mapeo de clase de rol para almacenar usuarios

--- src/main/java/mx/flvs/domain/Rol.java
+++ src/main/java/mx/flvs/domain/Rol.java
@@ -1,8 +1,11 @@
 package mx.flvs.domain;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Entity
@@ -10,5 +13,12 @@ import lombok.Data;
 @Table(name = "rol")
 public class Rol implements Serializable{
     
-    private static Long serialVersionUID = 1L
+    private static Long serialVersionUID = 1L;
+    
+    @Id
+    @Column(name = "id_rol")
+    private int idRol;
+    
+    @NotNull
+    private String nombre;
 }
