27eae965bbef66c4cb795538377d05626232fa69
Lazcano Valdez César Ricardo <la384541@uaeh.edu.mx>
2021-04-13 14:56:29 -0500

    Inicio del proyecto, Inclusión de dependencias

---
+++ src/test/java/mx/flvs/ControlClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.flvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
