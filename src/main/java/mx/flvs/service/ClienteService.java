7875f4e7d35e1014e2b84233d7c307f12bc828f3
Lazcano Valdez César Ricardo <la384541@uaeh.edu.mx>
2021-04-13 20:48:50 -0500

    Implementación de métodos para CRUD de clientes

--- src/main/java/mx/flvs/service/ClienteService.java
+++ src/main/java/mx/flvs/service/ClienteService.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mx.flvs.service;
 
-/**
- *
- * @author Bubu
- */
-public class ClienteService {
+import java.util.List;
+import mx.flvs.dao.ClienteDao;
+import mx.flvs.domain.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClienteService implements IService{
+    
+    @Autowired
+    private ClienteDao clienteDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cliente> listarClientes() {
+        return (List<Cliente>) clienteDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void guardar(Cliente cliente) {
+        clienteDao.save(cliente);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(Cliente cliente) {
+        clienteDao.delete(cliente);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente findById(Cliente cliente) {
+        return clienteDao.findById(cliente.getIdCliente()).orElse(null);
+    }
     
 }
