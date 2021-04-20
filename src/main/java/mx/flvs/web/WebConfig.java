e2d26df57932aeb32ec56714f60d222a4f9bd349
Lazcano Valdez César Ricardo <la384541@uaeh.edu.mx>
2021-04-20 14:14:19 -0500

    Configuraciones web de aplicación

---
+++ src/main/java/mx/flvs/web/WebConfig.java
@@ -0,0 +1,41 @@
+package mx.flvs.web;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang"); ////indica el nombre del parametro para cuando se indica el lenguaje en la vista
+        return lci;
+    }
+    
+     @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro){
+        registro.addViewController("/").setViewName("index");
+        registro.addViewController("/login");
+        registro.addViewController("/errores/403").setViewName("/errores/403");
+    }
+}
