825b71483de08870e317efabc41f036f2e4eae19
Lazcano Valdez César Ricardo <la384541@uaeh.edu.mx>
2021-04-20 14:20:28 -0500

    Controlador parte del MVC, incompleto aun

--- src/main/java/mx/flvs/web/Controlador.java
+++ src/main/java/mx/flvs/web/Controlador.java
@@ -1,9 +1,52 @@
 package mx.flvs.web;
 
+import mx.flvs.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Controlador {
     
+    
+    @Autowired
+    private ClienteService clienteService;
+    
+    @GetMapping("/")
+    public String inicio(Model model, @AuthenticationPrincipal User user){
+        var personas = personaService.listarPersonas();
+        model.addAttribute("personas", personas);
+        return "index";
+    }
+    
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(@Valid Persona persona, Errors errores){
+        if(errores.hasErrors()){
+            return "modificar";
+        }
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+    
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
+    }
     
 }
